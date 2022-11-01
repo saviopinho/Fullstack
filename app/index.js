5b60ed7541a2dfc7bf13e9472ff80530f2741a5e
SAVIO PINHO NUNES <savio.pinho@hotmail.com>
2022-11-01 06:50:32 -0300

    new

--- app/index.js
+++ app/index.js
@@ -6,25 +6,29 @@ const User = require('./models/users');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin','*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET','POST','PUT','DELETE');
-  next();
-})
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET',
+        'POST',
+        'PUT',
+        'DELETE'
+    );
+    next();
+});
 
 app.use('/dev', require('./routes/dev'));
 app.use('/users', require('./routes/users'));
 
-(async () =>{
-  try {
-    await sequelize.sync(
-      {force: false}
-    );
-    console.log("test");
-    app.listen(process.env.EXTERNAL_PORT || 3001);
-  } catch (error) {
-    console.error(error);
-  }
-})()
+(async () => {
+    try {
+        await sequelize.sync({ force: false });
+        console.log('test');
+        app.listen(process.env.EXTERNAL_PORT || 3001);
+    } catch (error) {
+        console.error(error);
+    }
+})();
